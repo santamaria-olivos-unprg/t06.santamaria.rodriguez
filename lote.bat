c1aa710e55ba5393f12828a3d6b84a655f2eb689
santamaria-olivos-unprg <57973991+santamaria-olivos-unprg@users.noreply.github.com>
2019-12-09 13:05:57 -0500

    condicional lote.bat

---
+++ lote.bat
@@ -0,0 +1,119 @@
+python santamaria/simple01.py Juan 1.65 60
+python santamaria/simple01.py Mario 1.45 60
+python santamaria/simple01.py Fabiola 1.35 40
+
+python santamaria/simple02.py Maria 358
+python santamaria/simple02.py Maria 300
+python santamaria/simple02.py Maria 254
+
+python santamaria/simple03.py 10
+python santamaria/simple03.py 15
+python santamaria/simple03.py 35
+
+python santamaria/simple04.py Maria 12 13 16
+python santamaria/simple04.py Carlos 11 12 15
+python santamaria/simple04.py Marcos 16 18 17
+
+python santamaria/simple05.py Ericka Huawei
+python santamaria/simple05.py Mario LG
+python santamaria/simple05.py Sofia SAMSUNG
+
+python santamaria/simple06.py 14
+python santamaria/simple06.py 2
+python santamaria/simple06.py 10
+
+python santamaria/simple07.py 15
+python santamaria/simple07.py 20
+python santamaria/simple07.py 41
+
+python santamaria/simple08.py A 14.5 B 20.5
+python santamaria/simple08.py C 18.5 D 16.5
+python santamaria/simple08.py E 12.5 F 10.5
+
+python santamaria/simple09.py Falla 18
+python santamaria/simple09.py Sanchez 15
+python santamaria/simple09.py Chapoñan 20
+
+python santamaria/simple10.py Laura 15
+python santamaria/simple10.py Fanny 18
+python santamaria/simple10.py Rosa 12
+
+python santamaria/doble01.py Juan 1.65 60
+python santamaria/doble01.py Mario 1.45 60
+python santamaria/doble01.py Fabiola 1.35 40
+
+python santamaria/doble02.py Maria 358
+python santamaria/doble02.py Maria 300
+python santamaria/doble02.py Maria 254
+
+python santamaria/doble03.py 10
+python santamaria/doble03.py 15
+python santamaria/doble03.py 35
+
+python santamaria/doble04.py Maria 12 13 16
+python santamaria/doble04.py Carlos 11 12 15
+python santamaria/doble04.py Marcos 16 18 17
+
+python santamaria/doble05.py Ericka Huawei
+python santamaria/doble05.py Mario LG
+python santamaria/doble05.py Sofia SAMSUNG
+
+python santamaria/doble06.py 14
+python santamaria/doble06.py 2
+python santamaria/doble06.py 10
+
+python santamaria/doble07.py 15
+python santamaria/doble07.py 20
+python santamaria/doble07.py 41
+
+python santamaria/doble08.py A 14.5 B 20.5
+python santamaria/doble08.py C 18.5 D 16.5
+python santamaria/doble08.py E 12.5 F 10.5
+
+python santamaria/doble09.py Falla 18
+python santamaria/doble09.py Sanchez 15
+python santamaria/doble09.py Chapoñan 20
+
+python santamaria/doble10.py Laura 15
+python santamaria/doble10.py Fanny 18
+python santamaria/doble10.py Rosa 12
+
+python santamaria/multiple01.py Juan 1.65 60
+python santamaria/multiple01.py Mario 1.45 60
+python santamaria/multiple01.py Fabiola 1.35 40
+
+python santamaria/multiple02.py Maria 358
+python santamaria/multiple02.py Maria 300
+python santamaria/multiple02.py Maria 254
+
+python santamaria/multiple03.py 10
+python santamaria/multiple03.py 15
+python santamaria/multiple03.py
+
+python santamaria/multiple04.py Maria 12 13 16
+python santamaria/multiple04.py Carlos 11 12 15
+python santamaria/multiple04.py Marcos 16 18 17
+
+python santamaria/multiple05.py Ericka Huawei
+python santamaria/multiple05.py Mario LG
+python santamaria/multiple05.py Sofia SAMSUNG
+
+python santamaria/multiple06.py 14
+python santamaria/multiple06.py 2
+python santamaria/multiple06.py 10
+
+python santamaria/multiple07.py 15
+python santamaria/multiple07.py 20
+python santamaria/multiple07.py 41
+
+python santamaria/multiple08.py A 14.5 B 20.5
+python santamaria/multiple08.py C 18.5 D 16.5
+python santamaria/multiple08.py E 12.5 F 10.5
+
+python santamaria/multiple09.py Falla 18
+python santamaria/multiple09.py Sanchez 15
+python santamaria/multiple09.py Chapoñan 20
+
+python santamaria/multiple10.py Laura 15
+python santamaria/multiple10.py Fanny 18
+python santamaria/multiple10.py Rosa 12
